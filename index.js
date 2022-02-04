341343c1d2414817b5476edc75c1f41300f90511
lorenzoleite <lorenzokleite@gmail.com>
2022-02-04 20:12:24 -0300

    🚧 UI Table Responses in Progress

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const analisadorLexico = CreateAnalisadorLexico()
 const inputFile = document.getElementById('inputFile')
 const saida = document.getElementById('output')
 const button = document.getElementById('btn-compilar')
+const tableBody = document.getElementById('tableBody')
 
 let codigo
 let lines
@@ -24,6 +25,21 @@ button.addEventListener('click', function () {
   while (true) {
     const tokenRetornado = analisadorLexico.scanner()
     console.log(tokenRetornado)
+    let tdClasse = document.createElement('td')
+    let tdLexema = document.createElement('td')
+    let tdTipo = document.createElement('td')
+
+    let responseClasse = document.createTextNode(tokenRetornado.classe)
+    let responseLexema = document.createTextNode(tokenRetornado.lexema)
+    let responseTipo = document.createTextNode(tokenRetornado.tipo)
+
+    tdClasse.appendChild(responseClasse)
+    tdLexema.appendChild(responseLexema)
+    tdTipo.appendChild(responseTipo)
+
+    tableBody.appendChild(tdClasse)
+    tableBody.appendChild(tdLexema)
+    tableBody.appendChild(tdTipo)
     if (tokenRetornado.lexema === '$') {
       break
     }
